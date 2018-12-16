dc868c8afcd471e216a5a887035c665a8750923e
Hadi Nur Wahid <hadiykk99@gmail.com>
2018-12-16 16:36:37 +0700

    opening3

--- screens/opening/OpeningScreen.js
+++ screens/opening/OpeningScreen.js
@@ -2,11 +2,13 @@
 
 import React, { Component } from 'react';
 
+
 import {
   StyleSheet,
   Button,
   View,
   TouchableHighlight,
+  TouchableOpacity,
   Image,
   AppRegistry,
   ActivityIndicator,
@@ -17,52 +19,96 @@ import {
 
 // import { Examples } from '@shoutem/ui';
 
+const logo = () => {
+  return (
+    
+      <Text>Logo</Text>
+    
+  );
+}
+
+const profile = () => {
+  return (
+      <Button title="Profile"/>
+  );
+}
+
 export default class Opening extends Component {
+
+  selectLevel = () => {
+    alert('select level')
+  }
+
+ static navigationOptions = {
+    headerTitle: logo,
+    headerLeft: logo,
+    headerRight: (<Button title="Profile"/>),
+    headerStyle: {
+      backgroundColor: '#FFFFFF',
+    }
+  };
 
   render() {
     return (
       <View style={Styles.container}>
-            <View style={Styles.header}>
-                <View style={Styles.logo}>
-                  <Text>Logo</Text>
-                </View>
-                <View style={Styles.profile}>
-                  <Text>Profile</Text>
-                </View>
-            </View>
+              {/* <View style={Styles.header}>
+                  <View style={Styles.logo}>
+                    <Text>Logo</Text>
+                  </View>
+                  <View style={Styles.profile}>
+                    <Text>Profile</Text>
+                  </View>
+              </View> */}
+             <TouchableOpacity style={Styles.level} onPress={() => this.selectLevel('111')}> 
+              <View style={Styles.level}>
+                  <View style={Styles.levelTextView}>
+                      <Text style={Styles.levelText}>Easy</Text>
+                  </View>
+                  <View style={{flex:1, flexDirection:'row', justifyContent: 'flex-start'}}>
+                    <Image 
+                      source={{uri: 'https://images.pexels.com/photos/316465/pexels-photo-316465.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'}}
+                      style={{width: 100, height: 150, margin: 10}}  
+                    />
+                  <View style={Styles.levelDescriptionView}>
+                    <Text style={Styles.levelDescriptionText}>this is the</Text>
+                  </View>      
+                  </View>     
+              </View>
+            </TouchableOpacity>
 
-            <View style={Styles.level}>
-                <View style={Styles.levelTextView}>
-                  <Text style={Styles.levelText}>Easy</Text>
-                </View>
-                <View style={{flex:1, flexDirection:'row', justifyContent: 'flex-start'}}>
-                <Image 
-                  source={{uri: 'https://images.pexels.com/photos/316465/pexels-photo-316465.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'}}
-                  style={{width: 100, height: 150, margin: 10}}  
-                />
-                 <View style={Styles.levelDescriptionView}>
-                  <Text style={Styles.levelDescriptionText}>this is the desvription lorem ioncsjk hdsfh</Text>
-                </View>      
-                </View>            
-            </View>
+            <TouchableOpacity style={Styles.level} onPress={() => this.selectLevel('111')}> 
+              <View style={Styles.level}>
+                  <View style={Styles.levelTextView}>
+                      <Text style={Styles.levelText}>Normal</Text>
+                    </View>
+                    <View style={{flex:1, flexDirection:'row', justifyContent: 'flex-start'}}>
+                    <Image 
+                      source={{uri: 'https://images.pexels.com/photos/316465/pexels-photo-316465.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'}}
+                      style={{width: 100, height: 150, margin: 10}}  
+                    />
+                    <View style={Styles.levelDescriptionView}>
+                      <Text style={Styles.levelDescriptionText}>this is the</Text>
+                    </View>      
+                  </View>     
+              </View>
+            </TouchableOpacity>
 
-            <View style={Styles.level}>
-                <View style={Styles.levelTextView}>
-                  <Text style={Styles.levelText}>Medium</Text>
-                </View>
-                <View style={Styles.levelDescriptionView}>
-                  <Text style={Styles.levelDescriptionText}>this is the desvription lorem ioncsjk hdsfh</Text>
-                </View>
-            </View>
-
-            <View style={Styles.level}>
-                <View style={Styles.levelTextView}>
-                  <Text style={Styles.levelText}>Hard</Text>
-                </View>
-                <View style={Styles.levelTextView}>
-                  <Text style={Styles.levelDescriptionText}>Draw The World's 2nd Favorite domestic animals</Text>
-                </View>
-            </View>
+            <TouchableOpacity style={Styles.level} onPress={() => this.selectLevel('111')}> 
+              <View style={Styles.level}>
+                  <View style={Styles.levelTextView}>
+                      <Text style={Styles.levelText}>Hard</Text>
+                    </View>
+                    <View style={{flex:1, flexDirection:'row', justifyContent: 'flex-start'}}>
+                    <Image 
+                      source={{uri: 'https://images.pexels.com/photos/316465/pexels-photo-316465.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'}}
+                      style={{width: 100, height: 150, margin: 10}}  
+                    />
+                    <View style={Styles.levelDescriptionView}>
+                      <Text style={Styles.levelDescriptionText}>this is the</Text>
+                    </View>      
+                  </View>     
+              </View>
+            </TouchableOpacity>
         </View>
 
     );
@@ -90,9 +136,12 @@ const Styles = StyleSheet.create({
   },
   level: {
     flex: 3,
-    backgroundColor: 'green',
+    backgroundColor: '#F8F8F8',
     margin: 5,
-    borderRadius: 15   
+    borderRadius: 15,   
+    borderColor: '#F8F8F8',
+    // underlayColor: '#F8F8F8'
+    // activeOpacity: 1
   },
   levelTextView: {
     flexDirection: 'row',
@@ -101,7 +150,8 @@ const Styles = StyleSheet.create({
   levelText: {
     color: 'crimson',
     fontSize: 15,
-    margin: 10,
+    margin: 10, 
+    fontFamily: 'Helvetica'
   },
   levelDescriptionView: {
     flex:1,
